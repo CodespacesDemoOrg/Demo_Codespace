f333b09daa0b16de947cbf8fc2a9720b24e7fbde
VarshiniAdiga123 <113126638+VarshiniAdiga123@users.noreply.github.com>
2022-09-26 19:46:35 +0530

    first commit

---
+++ libs/wxp-core/dist/src/utilities/ILogger.d.ts
@@ -0,0 +1,59 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2022 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
+import { LoggerOptions } from "../models/index.js";
+/**
+ * Logger interface.
+ */
+export interface ILogger {
+    /**
+     * Log message.
+     *
+     * @param message - Message to log.
+     * @param options - Logger {@link LoggerOptions} options.
+     */
+    message(message: string, options?: LoggerOptions): void;
+    /**
+     * Log information message.
+     *
+     * @param message - Message to log.
+     * @param options - Logger {@link LoggerOptions} options.
+     */
+    information(message: string, options?: LoggerOptions): void;
+    /**
+     * Log success message.
+     *
+     * @param message - Message to log.
+     * @param options - Logger {@link LoggerOptions} options.
+     */
+    success(message: string, options?: LoggerOptions): void;
+    /**
+     * Log warning message.
+     *
+     * @param message - Message to log.
+     * @param options - Logger {@link LoggerOptions} options.
+     */
+    warning(message: string, options?: LoggerOptions): void;
+    /**
+     * Log error message.
+     *
+     * @param message - Message to log.
+     * @param options - Logger {@link LoggerOptions} options.
+     */
+    error(message: unknown, options?: LoggerOptions): void;
+}
+//# sourceMappingURL=ILogger.d.ts.map
