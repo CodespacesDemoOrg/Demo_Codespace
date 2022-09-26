925b3ea25317ddbcb21f0b7fcc1b95fb312b625f
VarshiniAdiga123 <113126638+VarshiniAdiga123@users.noreply.github.com>
2022-09-26 19:49:14 +0530

    first commit

---
+++ libs/wxp-scripts/dist/src/validators/IScriptValidator.d.ts
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2022 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
+import { Script } from "../models/index.js";
+/**
+ * Script validator interface.
+ */
+export interface IScriptValidator {
+    /**
+     * Validate the script.
+     *
+     * @param script - Script to run.
+     */
+    validateScript(script: Script): void;
+    /**
+     * Validate the Document URL provided as an option to the script.
+     *
+     * @param script - Script to run.
+     * @param documentUrl - Spice Document URL where the Add-In needs to be sideloaded.
+     */
+    validateDocumentURL(script: Script, documentUrl: string): void;
+}
+//# sourceMappingURL=IScriptValidator.d.ts.map
