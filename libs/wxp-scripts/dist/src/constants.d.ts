3786ec1807e41954dc0ae6b66893be0917c65374
VarshiniAdiga123 <113126638+VarshiniAdiga123@users.noreply.github.com>
2022-09-26 19:48:12 +0530

    first commit

---
+++ libs/wxp-scripts/dist/src/constants.d.ts
@@ -0,0 +1,29 @@
+/*************************************************************************
+ *
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ *  Copyright 2022 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ **************************************************************************/
+export declare const PROGRAM_NAME = "wxp-scripts";
+export declare const EXTENSIONS_TO_TRANSPILE: Set<string>;
+export declare const HTTPS = "https";
+export declare const WSS = "wss";
+export declare const DEFAULT_SRC_DIRECTORY = "src";
+export declare const DEFAULT_OUTPUT_DIRECTORY = "dist";
+export declare const DEFAULT_HOST_NAME = "localhost";
+export declare const DEFAULT_PORT = "5241";
+export declare const QUERY_PARAM_HTTP_PORT_KEY = "devHTTPSPort";
+export declare const QUERY_PARAM_WS_PORT_KEY = "devWSSPort";
+export declare const MANIFEST_JSON = "manifest.json";
+//# sourceMappingURL=constants.d.ts.map
